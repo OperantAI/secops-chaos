13b0735730613574b4c13cc269a55b510531d958
Glenn McDonald <glenn.mcdonald@operant.ai>
2023-10-25 00:59:35 -0400

    Define consts for output colors

--- internal/output/output.go
+++ internal/output/output.go
@@ -8,23 +8,32 @@ import (
 	"github.com/charmbracelet/lipgloss/table"
 )
 
+const (
+	InfoColor        = lipgloss.Color("205")
+	SuccessColor     = lipgloss.Color("205")
+	WarningColor     = lipgloss.Color("205")
+	ErrorColor       = lipgloss.Color("205")
+	FatalColor       = lipgloss.Color("205")
+	TableBorderColor = lipgloss.Color("205")
+)
+
 func WriteInfo(msg string, args ...interface{}) {
-	style := lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	style := lipgloss.NewStyle().Foreground(InfoColor)
 	fmt.Println(style.Render(fmt.Sprintf(msg, args...)))
 }
 
 func WriteSuccess(msg string, args ...interface{}) {
-	style := lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	style := lipgloss.NewStyle().Foreground(SuccessColor)
 	fmt.Println(style.Render(fmt.Sprintf(msg, args...)))
 }
 
 func WriteWarning(msg string, args ...interface{}) {
-	style := lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	style := lipgloss.NewStyle().Foreground(WarningColor)
 	fmt.Println(style.Render(fmt.Sprintf(msg, args...)))
 }
 
 func WriteError(err error) {
-	style := lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	style := lipgloss.NewStyle().Foreground(ErrorColor)
 	fmt.Println(style.Render(err.Error()))
 }
 
@@ -36,7 +45,7 @@ func WriteFatal(err error) {
 func WriteTable(headers []string, rows [][]string) {
 	t := table.New().
 		Border(lipgloss.NormalBorder()).
-		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("205"))).
+		BorderStyle(lipgloss.NewStyle().Foreground(TableBorderColor)).
 		Headers(headers...).
 		Rows(rows...)
 
